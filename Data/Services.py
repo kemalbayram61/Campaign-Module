958eae504a93ee14e1e57219707e0781ac0d8f29
Kemal Bayram <36696235+kemalbayram61@users.noreply.github.com>
2022-06-18 17:33:36 +0300

    environment file initial operations complete

--- Data/Services.py
+++ Data/Services.py
@@ -1,8 +1,11 @@
+from Data.DBHelper import DBHelper
+from Objects.Campaign import Campaign
 class Services:
-    __envs :list[list[str]] = None
-
+    __envs :dict = None
+    __dbHelper: DBHelper = None
     def __init__(self):
         self.loadEnvs()
+        self.__dbHelper = DBHelper(self.__envs["CONNECTION_STRING"], self.__envs["DATABASE_NAME"])
 
     def getSelectedEnv(self) ->str:
         mainEnvFile = open("Config/main.env")
@@ -12,9 +15,8 @@ class Services:
         return "local"
 
     def loadEnvs(self):
-        self.__envs = []
+        self.__envs = {}
         envsFile = open("Config/" + self.getSelectedEnv() + ".env")
         for line in envsFile.readlines():
             if ("#" not in line):
-                self.__envs.append(line.split("=", 1))
-        print(self.__envs)
+                self.__envs[line.split("=", 1)[0]] = line.split("=", 1)[1]
