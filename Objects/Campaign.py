8facb48a5693e3abf48b3ad1ae91e55068b2cf94
Kemal Bayram <36696235+kemalbayram61@users.noreply.github.com>
2022-06-18 12:28:36 +0300

    update campaign and add conditional selection

--- Objects/Campaign.py
+++ Objects/Campaign.py
@@ -1,8 +1,10 @@
 from Abstracts.EProductFilter import ProductFilter
 from Objects.ProductFilterCriteria import ProductFilterCriteria
+from Objects.ConditionalSelection import ConditionalSelection
 class Campaign:
     __id: str
     __name: str
     __productFilter:ProductFilter
     __productFilterCriteria: ProductFilterCriteria
-    #todo bu adımdan sonrasını düzenle
+    __conditionalSelectionID: str
+    __conditionalSelectionObject: ConditionalSelection
