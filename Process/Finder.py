63606ea64e9eb7d932e90bbfb31e636312a6021e
Kemal Bayram <36696235+kemalbayram61@users.noreply.github.com>
2022-07-05 18:34:39 +0300

    update finder class

--- Process/Finder.py
+++ Process/Finder.py
@@ -1,9 +1,22 @@
 from Object.Basket import Basket
 from Object.Campaign import Campaign
+from Object.Product import Product
 
 class Finder:
     basked: Basket
     campaignList: list[Campaign]
 
     def discover_campaign_list(self) ->list[str]:
-        return []
+        productList: list[Product] = self.basked.productList
+        criteriaCampaignList: list[str] = []
+        actionCampaignList: list[str] = []
+        response: list[str] = []
+        for product in productList:
+            criteriaCampaignList = criteriaCampaignList + product.criteriaCampaignList
+            actionCampaignList = actionCampaignList + product.actionCampaignList
+
+        for criteria in criteriaCampaignList:
+            if(criteria in actionCampaignList):
+                response.append(criteria)
+
+        return response
