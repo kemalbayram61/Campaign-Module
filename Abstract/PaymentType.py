fca068d008c8138129e69693decfd0eb237628c7
Kemal Bayram <36696235+kemalbayram61@users.noreply.github.com>
2022-07-05 16:41:47 +0300

    update

--- Abstract/PaymentType.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-class PaymentType(Enum):
-    CASH = 0
-    CHECKS = 1
-    DEBIT_CARDS = 2
-    CREDIT_CARDS = 3
-    MOBILE_PAYMENTS = 4
-    ELECTRONIC_BANK_TRANSFERS = 5
--- Object/Basket.py
+++ Object/Basket.py
@@ -1,20 +1,19 @@
 from Object.Product import Product
-from Abstract.PaymentType import PaymentType
 
 class Basket:
     id: str
     customerID: str
     productList: list[Product]
-    paymentType: PaymentType
+    paymentTypeID: str
     paymentChannelID: str
 
     def __init__(self, id:str = None,
                  customerID: str = None,
                  paymentChannelID: str = None,
                  productList: list[Product] = None,
-                 paymentType: PaymentType = None):
+                 paymentTypeID: str = None):
         self.id = id
         self.customerID = customerID
         self.productList = [] if productList == None else productList
-        self.paymentType = paymentType
+        self.paymentTypeID = paymentTypeID
         self.paymentChannelID = paymentChannelID
