07d64953e7e723913ccce7600dfb2be5cc22bfcc
Kemal Bayram <36696235+kemalbayram61@users.noreply.github.com>
2022-07-10 13:51:29 +0300

    update db_helper

--- Data/DBHelper.py
+++ Data/DBHelper.py
@@ -9,6 +9,25 @@ class DBHelper:
     def __init__(self):
         self.config = Config()
         self.open_connection()
+        if(self.config.get_reset_table_on_init()):
+            self.reset_tables()
+
+    def reset_tables(self) ->None:
+        self.cursor.execute("drop table if exists product")
+        self.cursor.execute("drop table if exists campaign")
+        product_sql: str = '''
+            create table product(
+            id SERIAL NOT NULL PRIMARY KEY,
+            name varchar(60) NOT NULL,
+            barcode varchar(20) NOT NULL,
+            property json NOT NULL,
+            criteria varchar(100),
+            action varchar(100),
+            amount real
+            )
+        '''
+        self.cursor.execute(product_sql)
+        self.connection.commit()
 
     def select_one(self, table_name: str):
         self.cursor.execute("select * from " + table_name)
