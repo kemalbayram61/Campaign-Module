d495d587e6983f5cde3ec9af34a89fe421de773f
Kemal Bayram <36696235+kemalbayram61@users.noreply.github.com>
2022-07-11 11:13:18 +0300

    update payment channel object

--- Object/PaymentChannel.py
+++ Object/PaymentChannel.py
@@ -1,8 +1,11 @@
 class PaymentChannel:
     id: str
     name: dict # {"TR": "Türkçe tanım", "EN": "English Definition"}
+    campaignList: list[str]
 
     def __init__(self, id: str = None,
-                 name: dict = None):
+                 name: dict = None,
+                 campaignList: list[str] = None):
         self.id = id
         self.name = {} if name == None else name
+        self.campaignList = [] if campaignList == None else campaignList
