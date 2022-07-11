7e38573d90ada130709a7624029d733201eb6fff
Kemal Bayram <36696235+kemalbayram61@users.noreply.github.com>
2022-07-11 11:59:06 +0300

    update main

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
 from Data.ProductHelper import ProductHelper
 from Data.CustomerHelper import CustomerHelper
+from Data.PaymentTypeHelper import PaymentTypeHelper
+from Data.PaymentChannelHelper import PaymentChannelHelper
+from Data.DBHelper import DBHelper
+from Data.Config import Config
 from Mock.Product import ProductMock
 from Mock.Customer import CustomerMock
 from Mock.PaymentType import PaymentTypeMock
 from Mock.PaymentChannel import PaymentChannelMock
-from Data.DBHelper import DBHelper
-from Data.Config import Config
 from Object.Basket import Basket
 from Object.Product import Product
 from Object.Customer import Customer
+from Object.PaymentType import PaymentType
+from Object.PaymentChannel import PaymentChannel
 from Process.Finder import Finder
+from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 class RequestProduct(BaseModel):
     id:str
     qty: int
+
 
 class Request(BaseModel):
     id: str
@@ -24,10 +30,14 @@ class Request(BaseModel):
     paymentTypeID: str
     paymentChannelID: str
 
+
 config = Config()
 db_helper = DBHelper()
-if (config.get_reset_table_on_init()):
+
+
+if config.get_reset_table_on_init():
     db_helper.reset_tables()
+
 
 #add mock
 product_mock = ProductMock()
@@ -41,12 +51,13 @@ db_helper.execute_command(payment_type_mock.get_mock_sql())
 
 app = FastAPI()
 
+
 def get_basked(request: Request)->Basket:
     productList: list[Product] = []
     for product_req in request.productList:
         product_helper = ProductHelper(product_req.id)
         product = product_helper.get()
-        if(product is not None):
+        if product is not None:
             product.qty = product_req.qty
             productList.append(product)
 
@@ -57,11 +68,18 @@ def get_basked(request: Request)->Basket:
 
     return basket
 
+
 @app.post("/find_campaign_list")
 def find_campaign_list(request: Request):
     basket: Basket = get_basked(request)
     customer_helper: CustomerHelper = CustomerHelper(request.customerID)
+    payment_type_helper: PaymentTypeHelper = PaymentTypeHelper(request.paymentTypeID)
+    payment_channel_helper: PaymentChannelHelper = PaymentChannelHelper(request.paymentChannelID)
     customer: Customer = customer_helper.get()
+    payment_type: PaymentType = payment_type_helper.get()
+    payment_channel: PaymentChannel = payment_channel_helper.get()
     finder: Finder = Finder(customer=customer,
-                            basket=basket)
+                            basket=basket,
+                            payment_type=payment_type,
+                            payment_channel=payment_channel)
     return finder.discover_campaign_list()
