9ef96bcbee37944a72b356be98735d900e04962c
Kemal Bayram <36696235+kemalbayram61@users.noreply.github.com>
2022-07-15 17:19:44 +0300

    regulation mock data

--- Mock/Product.py
+++ Mock/Product.py
@@ -6,14 +6,14 @@ class ProductMock(MockObject):
         response = '''
         INSERT INTO public.product
         ("name", "barcode", "property", "criteria", "action", "amount")
-        VALUES('Tükenmez Kalem', '283746328746123', '{"Kategori":"Kırtasiye", "Alt Kategori": "Kalem"}', 'c1,c5', NULL, 50);
+        VALUES('Tükenmez Kalem', '283746328746123', '{"Kategori":"Kırtasiye", "Alt Kategori": "Kalem"}', '1,5', NULL, 50);
         
         INSERT INTO public.product
         ("name", "barcode", "property", "criteria", "action", "amount")
-        VALUES('Resim Defteri', '283746328746124', '{"Kategori":"Kırtasiye", "Alt Kategori": "Defter"}', 'c1,c8', 'c1,c2', 25);
+        VALUES('Resim Defteri', '283746328746124', '{"Kategori":"Kırtasiye", "Alt Kategori": "Defter"}', '1,8', '1,2', 25);
         
         INSERT INTO public.product
         ("name", "barcode", "property", "criteria", "action", "amount")
-        VALUES('Silgi', '283746328746124', '{"Kategori":"Kırtasiye", "Alt Kategori": "Silgi"}', 'c3,c8', NULL, 10);
+        VALUES('Silgi', '283746328746124', '{"Kategori":"Kırtasiye", "Alt Kategori": "Silgi"}', '3,8', NULL, 10);
         '''
         return response
