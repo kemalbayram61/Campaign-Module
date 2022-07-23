9c33ec3f51020612ad4ebe040d5c46a4286b1789
Kemal Bayram <36696235+kemalbayram61@users.noreply.github.com>
2022-07-23 16:41:34 +0300

    update product object

--- Object/Product.py
+++ Object/Product.py
@@ -8,6 +8,7 @@ class Product:
     criteria_campaign_list: list[str]
     action_campaign_list: list[str]
     is_used: bool
+    discount_amount: float
 
     def __init__(self, id: str = None,
                  name: str = None,
@@ -17,13 +18,15 @@ class Product:
                  amount: float = None,
                  is_used: bool = None,
                  criteria_campaign_list: list[str] = None,
-                 action_campaign_list: list[str] = None):
+                 action_campaign_list: list[str] = None,
+                 discount_amount: float = None):
         self.id = id
         self.name = name
         self.barcode = barcode
         self.qty = qty
         self.unit_price = unit_price
         self.amount = amount
-        self.is_used = is_used
+        self.is_used = False if is_used is None else is_used
         self.criteria_campaign_list = [] if criteria_campaign_list is None else criteria_campaign_list
         self.action_campaign_list = [] if action_campaign_list is None else action_campaign_list
+        self.discount_amount = discount_amount
