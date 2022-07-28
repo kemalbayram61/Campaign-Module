239cbdd8cde637c65fd4d32607ac49d5fb8d3196
Kemal Bayram <36696235+kemalbayram61@users.noreply.github.com>
2022-07-28 18:52:46 +0300

    update response object

--- Object/Basket.py
+++ Object/Basket.py
@@ -2,18 +2,18 @@ from Object.Product import Product
 
 
 class Basket:
-    id: str
+    order_id: str
     customer_id: str
     product_list: list[Product]
     payment_type_id: str
     payment_channel_id: str
 
-    def __init__(self, id: str = None,
+    def __init__(self, order_id: str = None,
                  customer_id: str = None,
                  payment_channel_id: str = None,
                  product_list: list[Product] = None,
                  payment_type_id: str = None):
-        self.id = id
+        self.order_id = order_id
         self.customer_id = customer_id
         self.product_list = [] if product_list is None else product_list
         self.payment_type_id = payment_type_id
