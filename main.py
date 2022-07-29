07cffcf80ce0272b477ccf7aa4f07aaafe79ac20
Kemal Bayram <36696235+kemalbayram61@users.noreply.github.com>
2022-07-29 15:09:07 +0300

    add org_code in request

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ class RequestBasketLine(BaseModel):
 
 class RequestBasket(BaseModel):
     order_id: str
+    org_id: str
     customer_id: str
     basket_lines: list[RequestBasketLine]
     payment_type_id: str
