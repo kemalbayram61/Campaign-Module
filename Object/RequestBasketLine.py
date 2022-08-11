be2cec150e1a5ff189a5dd708035b8a7405c28ca
Kemal Bayram <36696235+kemalbayram61@users.noreply.github.com>
2022-08-11 19:12:52 +0300

    bug fix

--- Object/RequestBasketLine.py
+++ Object/RequestBasketLine.py
@@ -5,4 +5,4 @@ class RequestBasketLine(BaseModel):
     qty: int
     barcode: str
     amount: float
-    unit_price: int
+    unit_price: float
--- Object/ResponseBasketLine.py
+++ Object/ResponseBasketLine.py
@@ -2,7 +2,7 @@ class ResponseBasketLine:
     qty: int
     barcode: str
     amount: float
-    unit_price: int
+    unit_price: float
     discount_amount: float
     line_amount: float
     discount_lines: list[dict]
